2a24b4e62f8b7eea2417c672df527b815b847a71
Esli Rodrigues <eslicdm1@gmail.com>
2025-07-14 09:14:23 -0300

    Initial commit

---
+++ src/main/java/com/eslirodrigues/tutorialjavabackend/TutorialJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eslirodrigues.tutorialjavabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialJavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialJavaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eslirodrigues/tutorialjavabackend/TutorialJavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eslirodrigues.tutorialjavabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialJavaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
